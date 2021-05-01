9421e31e39561aebfb6e81368cdccdd133320c3d
naiyagpatel <naiyagpatel@yahoo.co.uk>
2021-05-01 09:15:59 -0700

    Removed unwanted files from project structure

---
+++ src/features/categoriesSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loadCategories = createAsyncThunk(
+  "categories/loadCategories",
+  async () => {
+    const response = await fetch("https://www.reddit.com/r/.json");
+    const json = await response.json();
+    console.log(json);
+    //return json;
+  }
+);
+
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: {
+    categories: [],
+    isLoadingCategories: false,
+    failedToLoadCategories: false,
+  },
+  extraReducers: {
+    ["loadCategories.pending"]: (state, action) => {
+      state.isLoadingCategories = true;
+      state.failedToLoadCategories = false;
+    },
+    ["loadCategories.fulfilled"]: (state, action) => {},
+
+    ["loadCategories.rejected"]: (state, action) => {
+      state.isLoadingCategories = false;
+      state.failedToLoadCategories = true;
+    },
+  },
+});
